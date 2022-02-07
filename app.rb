3c749dbc9c7682361762833585d38392c69da23f
sapna tavrawala <sapnatavrawala@outlook.com>
2022-02-07 17:17:39 +0000

    bookmark class

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   configure :development do
@@ -14,12 +15,7 @@ class BookmarkManager < Sinatra::Base
  end
 
  get '/bookmarks' do
-   @bookmarks = [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-      ]
-    
+   @bookmarks = Bookmark.all
    erb :'bookmarks/index'
  end
 
