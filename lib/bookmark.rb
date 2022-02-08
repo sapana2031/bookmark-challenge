9fa74bf092a297b70c5943262164e5371d0dcc45
sapna tavrawala <sapnatavrawala@outlook.com>
2022-02-08 17:21:24 +0000

    09_setting_up_a_testing_environment

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -7,8 +7,13 @@ class Bookmark
     #   "http://www.destroyallsoftware.com",
     #   "http://www.google.com"
     # ]
-    connection = PG.connect(dbname: 'bookmark-manager')
-    result = connection.exec('SELECT * FROM bookmarks')
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark-manager')
+    end
+
+    result = connection.exec("SELECT * FROM bookmarks")
     result.map { |bookmark| bookmark['url'] }
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,10 +1,30 @@
+# in spec/features/viewing_bookmarks_spec.rb
+require 'pg'
+
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
-    save_and_open_page
 
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.destroyallsoftware.com"
     expect(page).to have_content "http://www.google.com"
   end
 end
+
+# feature 'Viewing bookmarks' do
+#   scenario 'A user can see bookmarks' do
+#     visit('/bookmarks')
+#     save_and_open_page
+
+#     expect(page).to have_content "http://www.makersacademy.com"
+#     expect(page).to have_content "http://www.destroyallsoftware.com"
+#     expect(page).to have_content "http://www.google.com"
+#   end
+# end
