520183ddfc134b9c7129fdd65b832a8116a26376
sapna tavrawala <sapnatavrawala@outlook.com>
2022-02-10 17:48:52 +0000

    wrapping data & securing app

--- app.rb
+++ app.rb
@@ -26,12 +26,14 @@ class BookmarkManager < Sinatra::Base
  end
 
   post '/bookmarks' do
-    Bookmark.create(url: params[:url])
+    Bookmark.create(url: params[:url], title: params[:title])
     # url = params['url']
     # connection = PG.connect(dbname: 'bookmark_manager_test')
     # connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
     redirect '/bookmarks'
  end
+ 
+ 
 
 
  run! if app_file == $0
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,6 +1,15 @@
 require 'pg'
 
 class Bookmark
+
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
+
   def self.all
     connection = PG.connect(dbname: 'bookmark-manager')
     if ENV['ENVIRONMENT'] == 'test'
@@ -10,17 +19,30 @@ class Bookmark
     end
 
    result = connection.exec("SELECT * FROM bookmarks")
-   result.map { |bookmark| bookmark['url'] }
+   result.map do  |bookmark|
+   Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
   end
+end
+
   
-  def self.create(url:)
+  def self.create(url:, title:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
-      connection = PG.connect(dbname: 'bookmark_manager')
+      connection = PG.connect(dbname: 'bookmark-manager')
     end
+
+    result = connection.exec_params(
+      # The first argument is our SQL query template
+      # The second argument is the 'params' referred to in exec_params
+      # $1 refers to the first item in the params array
+      # $2 refers to the second item in the params array
+      "INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
+    )
+  Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+
   
-    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    # connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")---- see changes above when we add result.
   end
 end
 # [
